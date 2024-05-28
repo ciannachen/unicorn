8522b8a5b759af095df0e5d64f63e7ea9b4bb346
cianna <cianna.au@gmail.com>
2024-05-28 19:36:00 +1000

    fix

--- Breakout/game.js
+++ Breakout/game.js
@@ -50,6 +50,26 @@ canvas.addEventListener("click", function(event) {
     }
 });
 
+canvas.addEventListener("touchstart", function(event) {
+    const buttonWidth = 10 * canvas.width / 100; // 10% of the canvas width
+    const buttonHeight = 5 * canvas.height / 100; // 5% of the canvas height
+    const buttonX = (canvas.width - buttonWidth) / 2;
+    const buttonY = (canvas.height / 2) + (canvas.height / 10); // Adjust position slightly
+    const rect = canvas.getBoundingClientRect();
+    const touch = event.touches[0];
+    const mouseX = touch.clientX - rect.left;
+    const mouseY = touch.clientY - rect.top;
+
+    if (
+        mouseX > buttonX &&
+        mouseX < buttonX + buttonWidth &&
+        mouseY > buttonY &&
+        mouseY < buttonY + buttonHeight
+    ) {
+        resetGame();
+    }
+});
+
 function resetGame() {
     gameOver = false;
     gameWon = false;
